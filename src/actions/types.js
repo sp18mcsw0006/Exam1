7ac2ff231dacb14f1d550cdbcd51c2b217669f56
MAJU STUDENT <majustudent@jinnah.edu>
2019-06-15 16:26:48 +0500

    final commit all working

--- src/actions/types.js
+++ src/actions/types.js
@@ -8,14 +8,3 @@ export const SIGN_USER = "sign_user";
 export const SIGN_USER_SUCCESS = "sign_user_success";
 export const SIGN_USER_FAIL = "sign_user_fail";
 
-export const EMPLOYEE_UPDATE = "employee_update";
-export const EMPLOYEE_CREATE = "employee_create";
-export const EMPLOYEES_FETCH_SUCCESS = "employee_fetch_success";
-
-
-export const LOGIN_USER_SUCCESSt = "login_user_successt";
-export const LOGIN_USER_FAILt = "login_user_failt";
-export const LOGIN_USERt = "login_usert";
-export const SIGN_USERt = "sign_usert";
-export const SIGN_USER_SUCCESSt = "sign_user_successt";
-export const SIGN_USER_FAILt = "sign_user_failt";
